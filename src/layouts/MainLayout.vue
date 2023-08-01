3483722fc5015bf25f1fba254df1bc423fd5f24e
xafable <xafable55@gmail.com>
2023-08-01 21:44:10 +0300

    deploy

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+
+    <q-footer elevated class="bg-grey-8 text-white" style="background: #492532 !important ;">
+      <q-toolbar>
+        <q-toolbar-title>
+
+          <div style="height: 6px;"></div>
+          <div class="row justify-center">
+            <q-btn round size="lg" color="primary" icon="fa-brands fa-telegram" style="margin-left: 5px;margin-right: 5px;"/>
+            <q-btn round size="lg" color="primary" icon="fa-brands fa-instagram" style="margin-left: 5px;margin-right: 5px;"/>
+            <q-btn round size="lg" color="primary" icon="fa-brands fa-discord" style="margin-left: 5px;margin-right: 5px;"/>
+          </div>
+          <div style="height: 6px;"></div>
+
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+
+  </q-layout>
+</template>
+<script setup>
+</script>
